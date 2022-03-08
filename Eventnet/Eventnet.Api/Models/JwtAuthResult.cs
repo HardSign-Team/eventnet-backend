ac1294faaa4478ef5dc27b62535406b192a62c47
DaGnYMilleR <danil.lunyow@mail.ru>
2022-03-09 00:05:35 +0500

    Remove unused attribute

--- Eventnet/Eventnet.Api/Models/JwtAuthResult.cs
+++ Eventnet/Eventnet.Api/Models/JwtAuthResult.cs
@@ -1,6 +1,5 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
-using System.Text.Json.Serialization;
 
 namespace Eventnet.Models;
 
-public record JwtAuthResult([property: JsonPropertyName("accessToken")] JwtSecurityToken AccessToken);
+public record JwtAuthResult(JwtSecurityToken AccessToken);
