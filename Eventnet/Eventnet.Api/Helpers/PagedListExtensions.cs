2e2c0e64438e4945ad99ddbd43e16c788836dd9b
gODealOAple <maksim.math@gmail.com>
2022-03-13 14:51:40 +0500

    Fix naming

--- Eventnet/Eventnet.Api/Helpers/PagedListExtensions.cs
+++ Eventnet/Eventnet.Api/Helpers/PagedListExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace Eventnet.Helpers;
 
-public static class PagedListExtensions3
+public static class PagedListExtensions
 {
     public static object ToPaginationHeader<T>(this PagedList<T> pagedList,
         Func<int, int, string?> generatePageLink) =>
