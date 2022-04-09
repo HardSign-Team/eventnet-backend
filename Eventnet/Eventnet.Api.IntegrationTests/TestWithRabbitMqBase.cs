ef79ca4b0abf8b5198f4d30040080ce7fcc73c0a
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2022-04-09 23:44:52 +0500

    add AbstractTestBase and TestWithRabbitMqBase

---
+++ Eventnet/Eventnet.Api.IntegrationTests/TestWithRabbitMqBase.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Eventnet.Api.IntegrationTests;
+
+public class TestWithRabbitMqBase : AbstractTestBase
+{
+    protected override WebApplicationFactory<Program> Factory { get; } = new RabbitMqTestFactory<Program>();
+}
--- Eventnet/Eventnet.Api.IntegrationTests/TestsBase.cs
+++ Eventnet/Eventnet.Api.IntegrationTests/TestsBase.cs
@@ -1,29 +1,8 @@
-﻿using System;
-using System.Net.Http;
-using Eventnet.DataAccess;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Eventnet.Api.IntegrationTests;
 
-public class TestsBase
+public class TestsBase : AbstractTestBase
 {
-    protected HttpClient HttpClient => factory.CreateClient();
-    private readonly WebApplicationFactory<Program> factory = new TestWebApplicationFactory<Program>();
-
-    protected void ApplyToDb(Action<ApplicationDbContext> action)
-    {
-        var scopeFactory = factory.Services.GetService<IServiceScopeFactory>();
-        using var scope = scopeFactory!.CreateScope();
-        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
-        action(context!);
-    }
-
-    protected T ApplyToDb<T>(Func<ApplicationDbContext, T> action)
-    {
-        var scopeFactory = factory.Services.GetService<IServiceScopeFactory>();
-        using var scope = scopeFactory!.CreateScope();
-        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
-        return action(context!);
-    }
+    protected override WebApplicationFactory<Program> Factory { get; } = new TestWebApplicationFactory<Program>();
 }
