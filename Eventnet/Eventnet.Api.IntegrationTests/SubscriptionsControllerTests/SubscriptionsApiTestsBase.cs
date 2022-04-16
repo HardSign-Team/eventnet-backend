13a382392475fabb73602ff45069520627e83466
gODealOAple <maksim.math@gmail.com>
2022-04-16 22:53:07 +0500

    Add tests for /api/subscriptions/count/{eventId} endpoint

---
+++ Eventnet/Eventnet.Api.IntegrationTests/SubscriptionsControllerTests/SubscriptionsApiTestsBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using Eventnet.Api.IntegrationTests.Helpers;
+
+namespace Eventnet.Api.IntegrationTests.SubscriptionsControllerTests;
+
+public class SubscriptionsApiTestsBase : TestsBase
+{
+    private const string BaseRoute = "/api/subscriptions";
+
+    protected Uri BuildCountOnEventUri(string? eventId)
+    {
+        var uriBuilder = new UriBuilder(Configuration.BaseUrl)
+        {
+            Path = $"{BaseRoute}/count/{HttpUtility.UrlEncode(eventId)}"
+        };
+        return uriBuilder.Uri;
+    }
+}
