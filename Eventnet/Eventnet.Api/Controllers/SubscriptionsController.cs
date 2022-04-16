0392673384785a46bdf47c745d0ba74a988e81cd
gODealOAple <maksim.math@gmail.com>
2022-04-16 08:51:37 +0500

    Define subscriptions controller interface

---
+++ Eventnet/Eventnet.Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventnet.Api.Controllers;
+
+[Route("api/subscriptions")]
+public class SubscriptionsController : Controller
+{
+    [Authorize]
+    [HttpPost("subscribe/{eventId}")]
+    public async Task<IActionResult> Subscribe(Guid eventId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [Authorize]
+    [HttpPost("unsubscribe/{eventId}")]
+    public async Task<IActionResult> UnSubscribe(Guid eventId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("count/{eventId}")]
+    public async Task<IActionResult> GetSubscriptionsCount(Guid eventId)
+    {
+        throw new NotImplementedException();
+    }
+}
