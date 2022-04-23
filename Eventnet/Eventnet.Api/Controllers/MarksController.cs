25b03327c329fb9113001f552ec76ff1aa88027e
gODealOAple <maksim.math@gmail.com>
2022-04-23 19:41:13 +0500

    Add interface of marks controller

---
+++ Eventnet/Eventnet.Api/Controllers/MarksController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventnet.Api.Controllers;
+
+[Route("api/marks")]
+public class MarksController : Controller
+{
+    [Authorize]
+    [HttpPost("likes/add/{eventId}")]
+    public async Task<IActionResult> AddLike(Guid eventId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [Authorize]
+    [HttpPost("likes/remove/{eventId}")]
+    public async Task<IActionResult> RemoveLike(Guid eventId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [Authorize]
+    [HttpPost("dislikes/add/{eventId}")]
+    public async Task<IActionResult> AddDislike(Guid eventId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [Authorize]
+    [HttpPost("dislikes/remove/{eventId}")]
+    public async Task<IActionResult> RemoveDislike(Guid eventId)
+    {
+        throw new NotImplementedException();
+    }
+}
