0ea785c757b7a9cda93bf11bff4e44a5750726ca
gODealOAple <maksim.math@gmail.com>
2022-05-11 17:43:50 +0500

    Add tests for saving event with tags in SaveToDbService

---
+++ Eventnet/Eventnet.Api.UnitTests/BaseTests.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Eventnet.Api.Config;
+using Eventnet.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eventnet.Api.UnitTests;
+
+public class BaseTests<T>
+{
+    private readonly DbContextOptions<ApplicationDbContext> contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+        .UseInMemoryDatabase(nameof(T) + "-db")
+        .Options;
+
+    protected IMapper CreateMapper()
+    {
+        var config = new MapperConfiguration(opt => opt.AddProfile<ApplicationMappingProfile>());
+        return config.CreateMapper();
+    }
+
+    protected ApplicationDbContext CreateDbContext() => new(contextOptions);
+}
