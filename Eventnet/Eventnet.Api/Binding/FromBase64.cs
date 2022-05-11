0276c8059bd907e64256068b60ca08bac083fdc2
gODealOAple <maksim.math@gmail.com>
2022-05-11 21:21:09 +0500

    Add /api/events/full endpoint and write some tests

---
+++ Eventnet/Eventnet.Api/Binding/FromBase64.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Eventnet.Api.Binding;
+
+public class Base64ModelBinderProvider<TModel> : IModelBinderProvider
+{
+    public IModelBinder? GetBinder(ModelBinderProviderContext context)
+    {
+        if (context == null)
+        {
+            throw new ArgumentNullException(nameof(context));
+        }
+
+        return context.Metadata.ModelType == typeof(TModel) ? new FromBase64Binder() : null;
+    }
+}
+
+public class FromBase64Binder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        if (bindingContext == null)
+            throw new ArgumentNullException(nameof(bindingContext));
+
+        var modelName = bindingContext.ModelName;
+        var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+
+        if (valueProviderResult == ValueProviderResult.None)
+            return Task.CompletedTask;
+
+        bindingContext.ModelState.SetModelValue(modelName, valueProviderResult);
+
+        var value = valueProviderResult.FirstValue;
+
+        if (string.IsNullOrEmpty(value))
+            return Task.CompletedTask;
+
+        try
+        {
+            var bytes = Convert.FromBase64String(value);
+            var json = Encoding.UTF8.GetString(bytes);
+            bindingContext.Result = ModelBindingResult.Success(JsonSerializer.Deserialize(json, bindingContext.ModelType));
+        }
+        catch (Exception)
+        {
+            bindingContext.Model = null;
+        }
+
+        return Task.CompletedTask;
+    }
+}
