da414065c217fc89b60cd2ceaf864057a31167c7
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2022-05-22 21:40:17 +0500

    change UpdateEventModel

--- Eventnet/Eventnet.Api/Models/Events/UpdateEventModel.cs
+++ Eventnet/Eventnet.Api/Models/Events/UpdateEventModel.cs
@@ -3,7 +3,11 @@
 namespace Eventnet.Api.Models.Events;
 
 public record UpdateEventModel(
-    string Username,
-    [EmailAddress] string Email,
-    [DataType(DataType.Password)] string Password,
-    [Phone] string? Phone);
+    [Required] Guid EventId,
+    [Required] DateTime StartDate,
+    DateTime? EndDate,
+    [Required] string Name,
+    [Required] string Description,
+    [Required] [Range(-90, 90)] double Longitude,
+    [Required] [Range(-90, 90)] double Latitude,
+    string[]? Tags);
