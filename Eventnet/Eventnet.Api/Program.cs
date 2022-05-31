bf68a5b6c5414cd79eb2e77f22a00642fee48d14
DaGnYMilleR <danil.lunyow@mail.ru>
2022-06-01 00:42:28 +0500

    return config to appsettings.json

--- Eventnet/Eventnet.Api/Program.cs
+++ Eventnet/Eventnet.Api/Program.cs
@@ -35,7 +35,6 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
     ContentRootPath = Directory.GetCurrentDirectory(),
     WebRootPath = "static"
 });
-builder.Configuration.AddJsonFile("config.json", false, true);
 
 var services = builder.Services;
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
